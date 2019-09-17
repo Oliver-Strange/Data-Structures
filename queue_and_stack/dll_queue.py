7d3bfc4cff59a824a1d194a0807bc298254530ca
Oliver-Strange <preston.burton@gmail.com>
2019-09-17 07:45:01 -0700

    forgot to commit, queue and stack are done

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,18 +1,24 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list')
+
 
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
 
-  def enqueue(self, value):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if len(self.storage) is not 0:
+            deleted = self.storage.remove_from_head()
+            self.size -= 1
+            return deleted
+        else:
+            return None
+
+    def len(self):
+        return self.size
